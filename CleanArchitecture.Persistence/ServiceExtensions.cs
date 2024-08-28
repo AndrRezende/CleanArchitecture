4c8d1978202923f6d370ed37082b3fcacd88f47a
Andre Rezende <andre.rezende1@outlook.com>
2024-08-28 16:32:45 -0300

    Criação de string de conexão, e configuração de arquivos de link

---
+++ CleanArchitecture.Persistence/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Persistence.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Persistence;
+
+public static class ServiceExtensions
+{
+    public static void ConfigurePersistenceApp(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Sqlite");
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IUserRepository, UserRepository>();
+    }
+}
