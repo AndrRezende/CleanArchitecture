e2cdb8ff2963b5363eba5eebc9c13e7622ed42f0
Andre Rezende <andre.rezende1@outlook.com>
2024-08-30 15:38:12 -0300

    criação de casos de uso para usuarios, validações

---
+++ CleanArchitecture.Application/UserCases/CreateUser/CreateUserMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.UserCases.CreatUser;
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Application.UserCases.CreateUser
+{
+    public sealed class CreateUserMapper : Profile
+    {
+        public CreateUserMapper() 
+        {
+            CreateMap<CreateUserRequest, User>();
+            CreateMap<User, CreateUserResponse>();
+        }
+    }
+}
---
+++ CleanArchitecture.Application/UserCases/CreateUser/CreateUserValidator.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchitecture.Application.UserCases.CreatUser;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.UserCases.CreateUser;
+
+public sealed class CreateUserValidator : AbstractValidator<CreateUserRequest>
+{
+    //Regras para inserir email e nome
+    public CreateUserValidator() 
+    {
+        RuleFor(X => X.Email).NotEmpty().MaximumLength(50).EmailAddress();
+        RuleFor(X => X.Name).NotEmpty().MinimumLength(3).MaximumLength(50);
+    }
+}
